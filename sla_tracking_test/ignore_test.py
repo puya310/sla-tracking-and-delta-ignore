318660dc85c3882916a0314ef02c1e86ce6317d2
puya310 <puya310r@gmail.com>
2023-12-16 17:30:11 -0500

    initial push

---
+++ sla_tracking_test/ignore_test.py
@@ -0,0 +1,119 @@
+import argparse
+import requests
+import json
+import os
+from datetime import datetime
+
+api_key = os.environ.get("SNYK_TOKEN")
+org_id = "0ebb9084-0c7b-4362-9a45-880e038d6284"
+endpoint_url = f"https://api.snyk.io/rest/orgs/{org_id}/audit_logs/search?version=2023-12-14%7Ebeta&size=100&event=org.project.ignore.create"
+
+
+# Function to fetch user details
+def get_user_details(user_id, api_key):
+    user_endpoint = f"https://api.snyk.io/rest/orgs/0ebb9084-0c7b-4362-9a45-880e038d6284/users/{user_id}?version=2023-12-14%7Ebeta"
+    user_headers = {
+        'Authorization': f"token {api_key}",
+        'Accept': 'application/vnd.api+json; charset=utf-8'
+    }
+    user_response = requests.get(user_endpoint, headers=user_headers)
+
+    if user_response.status_code == 200:
+        user_data = user_response.json()
+        attributes = user_data['data']['attributes']
+        user_name = attributes.get('name')
+        user_email = attributes.get('email')
+        return {'name': user_name, 'email': user_email}
+    else:
+        print(f"Error fetching user details. Status code: {user_response.status_code}")
+        return {'name': None, 'email': None}
+
+# Function to format creation time
+def format_creation_time(creation_time_iso):
+    creation_time_utc = datetime.fromisoformat(creation_time_iso[:-1]).strftime('%B %d, %Y at %H:%M')
+    return f"{creation_time_utc} ({creation_time_iso})"
+
+# Function to load JSON from file
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as json_file:
+            return json.load(json_file)
+    except FileNotFoundError:
+        return []
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Process Snyk API data.')
+parser.add_argument('--baseline', action='store_true', help='Generate baseline JSON file')
+parser.add_argument('--delta', metavar='DELTA_JSON_PATH', help='Compare with specified delta JSON file')
+args = parser.parse_args()
+
+# API endpoint
+headers = {
+    'Authorization': f"token {api_key}",
+    'Accept': 'application/vnd.api+json; charset=utf-8'
+}
+
+# Make a GET request to the API endpoint
+response = requests.get(endpoint_url, headers=headers)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+
+    # List to store the results
+    results_list = []
+
+    # Extract information for each item in the response
+    for item in data['data']['items']:
+        user_id = item['user_id']
+        issue_id = item['content']['issueId']
+        project_id = item['project_id']
+        creation_time_iso = item['created']
+        reason_for_ignore = item['content']['reasonType']
+
+        # Fetch user details
+        user_details = get_user_details(user_id, api_key)
+
+        # Format the CreationTime
+        creation_time = format_creation_time(creation_time_iso)
+
+        # Create a dictionary with the extracted information
+        result = {
+            'UserID': user_id,
+            'Name': user_details['name'],
+            'Email': user_details['email'],
+            'IssueID': issue_id,
+            'ProjectID': project_id,
+            'CreationTime': creation_time,
+            'ReasonForIgnore': reason_for_ignore
+        }
+
+        # Append the result to the list
+        results_list.append(result)
+
+    # If --baseline flag is provided, write results to a baseline JSON file
+    if args.baseline:
+        baseline_file_name = 'test1.json'
+        with open('test1.json', 'w') as json_file:
+            json.dump(results_list, json_file, indent=2)
+            print(f'Baseline JSON file created and saved as {baseline_file_name} - to compare for new ignores, run script with "--delta [filename]"')
+
+    # If --delta flag is provided, compare with the specified delta JSON file
+    elif args.delta:
+        delta_json = load_json(args.delta)
+        new_entries = [entry for entry in results_list if entry not in delta_json]
+        if new_entries:
+            print('New entries:')
+            print(json.dumps(new_entries, indent=2))
+        else:
+            print('No new entries found.')
+
+    # If no arguments provided, print the result in JSON format
+    else:
+        for result in results_list:
+            print(json.dumps(result, indent=2))
+
+else:
+    # Print an error message if the request was not successful
+    print(f"Error: Unable to fetch data. Status code: {response.status_code}")
---
+++ sla_tracking_test/sla_tracking.py
@@ -0,0 +1,61 @@
+import requests
+from datetime import datetime, timedelta
+import os
+
+# Replace {org_id} with your actual organization ID
+org_id = ""
+url = f"https://api.snyk.io/rest/orgs/{org_id}/issues?version=2023-12-14%7Ebeta&type=package_vulnerability&limit=100"
+api_key=os.environ.get("SNYK_TOKEN")
+
+# Make the API request
+
+headers = {
+    'Authorization': f"token {api_key}",
+    'Accept': 'application/vnd.api+json'
+}
+response = requests.get(url, headers=headers)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    try:
+        # Parse the JSON response
+        data = response.json().get('data', [])
+
+        # Input severity level (low, medium, high, critical)
+        selected_severity = input("Enter severity level (low, medium, high, critical): ").lower()
+
+        # Iterate through each entry in the 'data' array
+        for entry in data:
+            try:
+                issue_id = entry.get('id', 'N/A')
+                title = entry['attributes']['title']
+                created_at_str = entry['attributes']['created_at']
+                severity_level = entry['attributes']['effective_severity_level'].lower()
+                cwe = entry['attributes'].get('classes', [{}])[0].get('id', 'N/A')
+
+                # Convert the 'created_at' string to a datetime object
+                created_at = datetime.strptime(created_at_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+                # Calculate the current date and time
+                current_datetime = datetime.now()
+
+                # Check if the issue is more than 2 weeks old and matches the selected severity level
+                if current_datetime - created_at > timedelta(weeks=2) and severity_level in ('low', 'medium', 'high', 'critical'):
+                    if (selected_severity == 'low' and severity_level in ('low', 'medium', 'high', 'critical')) or \
+                       (selected_severity == 'medium' and severity_level in ('medium', 'high', 'critical')) or \
+                       (selected_severity == 'high' and severity_level in ('high', 'critical')) or \
+                       (selected_severity == 'critical' and severity_level == 'critical'):
+                        # Print the information in JSON format
+                        print(f"{{\"issueID\": \"{issue_id}\", \"title\": \"{title}\", \"CWE\": \"{cwe}\", \"Severity\": \"{severity_level}\", \"createdAt\": \"{created_at_str}\"}}")
+                else:
+                    print(f"Issue does not match criteria: {title}")
+
+            except KeyError as key_error:
+                print(f"KeyError: {key_error}. Unable to extract data from the response.")
+
+    except Exception as e:
+        # Print an error message if the request was not successful
+        print(f"Failed to fetch data. Error: {e}")
+else:
+    print(f"Failed to fetch data. Status code: {response.status_code}")
+
